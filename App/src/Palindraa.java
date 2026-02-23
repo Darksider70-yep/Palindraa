76d1b9becbceb6c0d7077482cbf0664c983dac92
Darksider70-yep <darksider70117@gmail.com>
2026-02-23 11:14:10 +0530

    UC5

--- App/src/Palindraa.java
+++ App/src/Palindraa.java
@@ -1,25 +1,25 @@
 import java.util.*;
 public class Palindraa {
     public static void main (String[] args){
-    System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Input TEXT:");
-    String input= sc.nextLine();
-    int n = input.length();
-    char[] chars = input.toCharArray();
-    int start = 0;
-    int end = n - 1;
-    boolean ispalindrome = true;
-    while(start < end){
-        if(chars[start] != chars[end]){
-            ispalindrome = false;
-            break;  }
-        start++;
-        end--;
-    }
-    if(ispalindrome){
-        System.out.println("The text is a palindrome");
-    }else{
-        System.out.println("The text is not a palindrome");}
+        System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input TEXT:");
+        String input= sc.nextLine();
+        Stack<Character>stack = new Stack<>();
+        for(char c : input.toCharArray()){
+            stack.push(c);
+        }
+        boolean ispalindrome = true;
+        for ( char c : input.toCharArray()){
+            if(stack.pop() != c){
+                ispalindrome = false;
+                break;
+            }
+        }
+        if(ispalindrome){
+            System.out.println("The text is a palindrome");
+        }else{
+            System.out.println("The text is not a palindrome");
+        }
     }
 }
