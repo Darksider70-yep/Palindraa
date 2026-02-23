80772cb54adc6d092df4660a99b49d715ea493f8
DAKSH GEHLOT <darksider70117@gmail.com>
2026-02-23 10:07:10 +0530

    Delete App directory

--- App/src/Palindraa.java
+++
@@ -1,5 +0,0 @@
-public class Palindraa {
-    public static void main (String[] args){
-    System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
-    }
-}
