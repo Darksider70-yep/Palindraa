1a9c5183669653ea4b02c055c0d1a62418227ea0
Darksider70-yep <darksider70117@gmail.com>
2026-02-23 11:26:26 +0530

    UC6

--- App/src/Palindraa.java
+++ App/src/Palindraa.java
@@ -1,25 +1,36 @@
 import java.util.*;
+
 public class Palindraa {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Input TEXT:");
-        String input= sc.nextLine();
-        Stack<Character>stack = new Stack<>();
-        for(char c : input.toCharArray()){
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
             stack.push(c);
+            queue.add(c);
         }
+
         boolean ispalindrome = true;
-        for ( char c : input.toCharArray()){
-            if(stack.pop() != c){
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 ispalindrome = false;
-                break;
+                break;  // optional optimization
             }
         }
-        if(ispalindrome){
+
+        if (ispalindrome) {
             System.out.println("The text is a palindrome");
-        }else{
+        } else {
             System.out.println("The text is not a palindrome");
         }
+
+        sc.close();
     }
 }
