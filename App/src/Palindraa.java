f819659b554ca9ca33fe33099225d5f3f23f4a8e
Darksider70-yep <darksider70117@gmail.com>
2026-02-23 10:39:57 +0530

    UC2

--- App/src/Palindraa.java
+++ App/src/Palindraa.java
@@ -1,5 +1,24 @@
+import java.util.*;
 public class Palindraa {
     public static void main (String[] args){
     System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input TEXT:");
+    String input= sc.nextLine();
+    int n = input.length();
+    boolean flag = false;
+    for(int i = 0; i < n/2; i++){
+        if( input.charAt(i) != input.charAt(n-1-i)){
+            break;
+        }
+        else {
+            flag = true;
+        }
+    }
+    if(flag){
+        System.out.println("The text is a palindrome");
+    } else {
+        System.out.println("The text is not a palindrome");
+    }
     }
 }
