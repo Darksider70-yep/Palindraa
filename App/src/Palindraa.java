9e04aaf9ccc89de2ec897545ae17f1ada3620160
Darksider70-yep <darksider70117@gmail.com>
2026-02-23 10:05:34 +0530

    first commit

---
+++ App/src/Palindraa.java
@@ -0,0 +1,5 @@
+public class Palindraa {
+    public static void main (String[] args){
+    System.out.println("#################\n Welcome\n   TO   \nPalindraa\n###################\nversion: 0.0.1");
+    }
+}
